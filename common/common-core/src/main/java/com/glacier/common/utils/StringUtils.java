10b2c6474de3cc255dd4cd229d66915ee1a937fc
glacier <glacier0315@126.com>
2020-02-13 11:06:04 +0800

    项目目录构建

---
+++ common/common-core/src/main/java/com/glacier/common/utils/StringUtils.java
@@ -0,0 +1,19 @@
+package com.glacier.common.utils;
+
+/**
+ * @author glacier
+ * @version 1.0
+ * @date 2020-02-13 11:00
+ */
+public class StringUtils {
+
+    /**
+     * 判断字符串为空或者空字符串
+     *
+     * @param str 输入字符串
+     * @return 判断结果
+     */
+    public static boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+}
